2597ae30f3874355e26949c93a66f9570d10163b
Haltes1225 <wiktor.jochymczyk@gmail.com>
2026-04-20 22:19:13 +0200

    test_store

---
+++ test_store.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import Select
+
+def test_store(selenium):
+    pass
