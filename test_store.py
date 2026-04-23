9efbb23940fbfbd5be2de035b9d74d3375cfefd1
Haltes1225 <wiktor.jochymczyk@gmail.com>
2026-04-23 19:18:43 +0200

    Popprawki do 2_4

--- test_store.py
+++ test_store.py
@@ -3,7 +3,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 def assert_amount(driver, search_phrase, amount):
-    driver.get("https://kodilla.com/pl/test/store")
+
+    phrase_type = type(search_phrase)
+    if not phrase_type is str:
+        raise TypeError(f"amount must be str type, is {phrase_type}")
+
+    amount_type = type(amount)
+    if not amount_type is int:
+        raise TypeError(f"amount must be int type, is {amount_type}")
+    
+    if not amount >= 0:
+        raise ValueError("amount must be a non negative integer")
+    
     wait=WebDriverWait(driver,60)
 
     search_bar = wait.until(
@@ -13,13 +24,14 @@ def assert_amount(driver, search_phrase, amount):
     search_bar.send_keys(search_phrase)
 
     wait.until(
-        EC.presence_of_element_located((By.XPATH, "//section/div[@id = 'elements-wrapper']"))
+        lambda d: len(d.find_elements(By.XPATH, "//section/div[@id = 'elements-wrapper']/div[@class = 'element']")) == amount
     )
     elements = driver.find_elements(By.XPATH, "//section/div[@id = 'elements-wrapper']/div[@class = 'element']")
-    
+
     assert len(elements) == amount
 
 def test_store(selenium):
+    selenium.get("https://kodilla.com/pl/test/store")
     assert_amount(selenium, "Laptop", 3)
     assert_amount(selenium, "NoteBook", 2)
     assert_amount(selenium, "Gaming", 1)
@@ -29,6 +41,9 @@ def test_store(selenium):
     assert_amount(selenium, "Alamakota", 0)
     assert_amount(selenium, "14", 1)
     assert_amount(selenium, "+", 0)
+    #assert_amount(selenium, 45, 32)
+    #assert_amount(selenium, "+", "9")
+    #assert_amount(selenium, "+", -1)
 
 
 
