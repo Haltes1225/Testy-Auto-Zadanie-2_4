e036506ff12daa5db6c39993a0e53f878262d7a3
Haltes1225 <wiktor.jochymczyk@gmail.com>
2026-05-01 11:15:50 +0200

    poprawki zad 2.5 v2

--- test_store_css.py
+++ test_store_css.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 def print_products_css(driver, search_phrase):
 
@@ -8,7 +9,7 @@ def print_products_css(driver, search_phrase):
     if not phrase_type is str:
         raise TypeError(f"amount must be str type, is {phrase_type}")
     
-    wait=WebDriverWait(driver,10)
+    wait=WebDriverWait(driver,5)
 
     search_bar = wait.until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "#searchField"))
@@ -16,15 +17,20 @@ def print_products_css(driver, search_phrase):
     search_bar.clear()
     search_bar.send_keys(search_phrase)
 
-    wait.until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#elements-wrapper > .element"))
-    )
-    elements = driver.find_elements(By.CSS_SELECTOR, "#elements-wrapper > .element")
+    try:
+        wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "#elements-wrapper > .element"))
+        )
+        elements = driver.find_elements(By.CSS_SELECTOR, "#elements-wrapper > .element")
 
-    print("\n")
-    for element in elements:
-        print(element.text)
         print("\n")
+        for element in elements:
+            print(element.text)
+            print("\n")
+    except TimeoutException:
+        print("No search results to print")
+
+
 
 def test_store_css(selenium):
     selenium.get("https://kodilla.com/pl/test/store")
